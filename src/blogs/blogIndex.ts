b53a4a92d4bb181b26aa155f30854f10ba39a0e1
d4niee <daniellambrecht10@gmail.com>
2025-03-28 17:42:53 +0100

    added entries

--- src/blogs/blogIndex.ts
+++ src/blogs/blogIndex.ts
@@ -1,4 +1,6 @@
 export const blogMap: Record<string, string> = {
     'demo': 'demo/demo.md',
+    'k3s-guide': 'k3s-kubernetes-guide/k3s-kubernetes-dashboard.md',
+    'gitops-challenges': 'gitops-thesis.md'
   };
   
